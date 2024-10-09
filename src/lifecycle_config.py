377de69c6754d18b9033c9a15f1fa7bf0cad5534
Taiki Maekawa <maekawataiki000@gmail.com>
2024-10-09 09:52:59 +0900

    fix: docker version (#6)

--- src/lifecycle_config.py
+++ src/lifecycle_config.py
@@ -88,7 +88,7 @@ if [ -z `which docker` ]; then
     sudo chmod a+r /etc/apt/keyrings/docker.asc
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt update -qq
-    sudo apt install -y -qq docker-ce docker-ce-cli docker-buildx-plugin docker-compose-plugin
+    sudo apt install -y -qq docker-ce=5:20.10.24~3-0~ubuntu-jammy docker-ce-cli=5:20.10.24~3-0~ubuntu-jammy docker-buildx-plugin docker-compose-plugin
 fi
 
 # install golang
